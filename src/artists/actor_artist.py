1cda74d3a576eea0570a102582da246863ed9815
mick-warehime <mickwarehime@gmail.com>
2019-12-22 15:33:29 -0800

    basic actor life visualization

--- src/artists/actor_artist.py
+++ src/artists/actor_artist.py
@@ -1,4 +1,5 @@
 from artists.artist import Artist
+from data.colors import WHITE
 from data.constants import SCREEN_SIZE
 from models.combat_model import CombatModel
 from views.pygame_screen import Screen
@@ -9,8 +10,16 @@ class ActorArtist(Artist):
     ENEMY_SIZE = (int(90 * ENEMY_SCALE), int(140 * ENEMY_SCALE))
     CHARACTER_SCALE = 4
     CHARACTER_SIZE = (int(32 * CHARACTER_SCALE), int(32 * CHARACTER_SCALE))
+    ENEMY_X = SCREEN_SIZE[0] - 500
+    ENEMY_Y = 200
+    CHAR_X = 300
+    CHAR_Y = 300
 
     def render(self, screen: Screen, model: CombatModel) -> None:
-        screen.render_enemy(model.skeleton.sprite,
-                            SCREEN_SIZE[0] - 500, 200, ActorArtist.ENEMY_SIZE)
-        screen.render_character(model.character.sprite, 300, 300, ActorArtist.CHARACTER_SIZE)
+        enemy_life_str = '{}/{}'.format(model.skeleton.hp, model.skeleton.max_hp)
+        screen.render_text(enemy_life_str, 30, ActorArtist.ENEMY_X, ActorArtist.ENEMY_Y-20, WHITE)
+        screen.render_enemy(model.skeleton.sprite, ActorArtist.ENEMY_X, ActorArtist.ENEMY_Y, ActorArtist.ENEMY_SIZE)
+
+        char_life_str = '{}/{}'.format(model.character.hp, model.character.max_hp)
+        screen.render_text(char_life_str, 30, ActorArtist.CHAR_X, ActorArtist.CHAR_X - 20, WHITE)
+        screen.render_character(model.character.sprite, ActorArtist.CHAR_X, ActorArtist.CHAR_Y, ActorArtist.CHARACTER_SIZE)
